a22dcdf96eb57df8216c88ebd2708d7f45e1392a
Francisco Pereyra <francisco.pereyra@brixit-solutions.com>
2013-05-20 01:26:26 -0300

    Avances en el framework php para el despliegue de páginas

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-	echo file_get_contents('template/main.php');
+	require_once 'core/page.php';
+	// echo file_get_contents('template/main.php');
+	
+	Page::addScriptFile('boostrap.js');
+	Page::addStyleFile('bootstrap.css');
+	Page::renderPage('main.php');
 ?>
