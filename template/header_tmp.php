3b48d14d219a559595a1abdfc43441b712a7aae0
Francisco Pereyra <francisco.pereyra@brixit-solutions.com>
2013-05-20 01:25:40 -0300

    Avances en el framework php para la gestión de páginas

---
+++ template/header_tmp.php
@@ -0,0 +1,22 @@
+<?php ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
+<title>Insert title here</title>
+
+<!-- Bootstrap Styles Inicio -->
+<link href="css/bootstrap.css" rel="stylesheet">
+<!-- Bootstrap Styles Fin -->
+
+<style type="text/css">
+			body {
+			padding-top: 60px;
+			padding-bottom: 0px;
+			padding-left: 20px;
+			padding-right: 20px;
+}
+</style>
+</head>
+
+<body>
